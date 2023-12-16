651be5171b6a88e409d317ddb6e5c05c7f1e4976
Souvik Rahut <souvikrahutofficial@gmail.com>
2023-12-16 19:03:09 +0530

    commit

--- src/Chapter_2_pre_ass.java
+++ src/Chapter_2_pre_ass.java
@@ -1,7 +1,7 @@
 public class Chapter_2_pre_ass {
     //QUICK QUIZ - Precedence and associativity
         public static void main(String[] args) {
-            int a=2,b=3,c=1,x=14,y=5,z=4,v=8,u=2,d=0;
+            int a=2,b=3,c=1,x=14,y=5,v=8,u=2,d=0;
             System.out.println((x-y)/2);
             System.out.println((b*b)-(4*a*c)/2*a);
             System.out.println(v*v-u*u);
