53425075114151a98ba785f8023dc71e92fd3c83
Souvik Rahut <souvikrahutofficial@gmail.com>
2023-12-16 19:47:05 +0530

    commit

--- src/Chapter_2_pre_ass.java
+++ src/Chapter_2_pre_ass.java
@@ -1,11 +1,15 @@
 public class Chapter_2_pre_ass {
-    //QUICK QUIZ - Precedence and associativity
+    /*QUICK QUIZ - Precedence and associativity
+                 - Increment and Decrement Operators  */
         public static void main(String[] args) {
             int a=2,b=3,c=1,x=14,y=5,v=8,u=2,d=0;
             System.out.println((x-y)/2);
             System.out.println((b*b)-(4*a*c)/2*a);
             System.out.println(v*v-u*u);
             System.out.println(a*b-d);
+            System.out.println(++y * 8);
+            char A = 'a';
+            System.out.println(++A);
         }
 
     }
